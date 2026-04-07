ea856cba856639242905e8f8d8e2b6511fc920b6
windevx <muhammadwinner25@gmail.com>
2026-04-07 14:57:56 +0700

    adding vars and method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int var1 = 100;
+int var2 = 67;
+
+static int Penjumlahan(int var1, int var2)
+{
+    return var1 + var2;
+}
+
+static int Pengurangan(int var1, int var2)
+{
+    return var1 - var2;
+}
+
+static int Perkalian(int var1, int var2)
+{
+    return var1 * var2;
+}
+
+static int Pembagian(int var1, int var2)
+{
+    return var1 / var2;
+}
+
+Console.WriteLine("Penjumlahan: " + var1 + " + " + var2 + " = " + Penjumlahan(var1, var2));
+Console.WriteLine("Pengurangan: " + var1 + " - " + var2 + " = " + Pengurangan(var1, var2));
+Console.WriteLine("Perkalian: " + var1 + " * " + var2 + " = " + Perkalian(var1, var2));
+Console.WriteLine("Pembagian: " + var1 + " / " + var2 + " = " + Pembagian(var1, var2));
