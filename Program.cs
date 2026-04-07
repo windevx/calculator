0a746cfb3f8a10c2b3835783a2b468e49d87d704
muhammad winner <muhammadwinner25@gmail.com>
2026-04-07 14:54:51 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ static int Pembagian(int var1, int var2)
 {
     return var1 / var2;
 }
-
+//test 123
 Console.WriteLine("Penjumlahan: " + var1 + " + " + var2 + " = " + Penjumlahan(var1, var2));
 Console.WriteLine("Pengurangan: " + var1 + " - " + var2 + " = " + Pengurangan(var1, var2));
 Console.WriteLine("Perkalian: " + var1 + " * " + var2 + " = " + Perkalian(var1, var2));
